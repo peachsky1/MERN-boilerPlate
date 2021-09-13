f3ab001340e82fb09c811a480316348bc7257113
jasonlee <jlee7060@gmail.com>
2021-09-13 13:26:36 -0400

    added post method for register

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
+
+const app = express();
+
+// https://stackoverflow.com/questions/24330014/bodyparser-is-deprecated-express-4
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// application/json
+app.use(express.json());
+const port = 3000;
 mongoose
     .connect(
-        "mongodb+srv://jasonlee:abcd1234@boilerplate.mal6y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        "mongodb+srv://jasonlee:password@boilerplate.mal6y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     )
     .then(() => console.log("Connected!"))
     .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+// register router
+app.post("/register", (req, res) => {
+    // adding client signup info into DB
+    const user = new User(req.body);
+    // mongoDB save
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true,
+        });
+    });
 });
 
 app.listen(port, () => {
