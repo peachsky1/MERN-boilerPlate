1c26c1f05811ffaa5d988c60fe85db91a4450d32
jasonlee <jlee7060@gmail.com>
2021-09-14 23:37:35 -0400

    setup proxy for cors policy and concurrently config in package.json

--- server/index.js
+++ server/index.js
@@ -13,81 +13,86 @@ app.use(express.urlencoded({ extended: true }));
 // application/json
 app.use(express.json());
 app.use(cookieParser());
-const port = 3000;
+
 mongoose
-    .connect(config.mongoURI)
-    .then(() => console.log("Connected!"))
-    .catch((err) => console.log(err));
+  .connect(config.mongoURI)
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
+});
+
+app.get("/api/hello", (req, res) => {
+  res.send("Hello!");
 });
 // register router
 app.post("/api/users/register", (req, res) => {
-    // adding client signup info into DB
-    const user = new User(req.body);
+  // adding client signup info into DB
+  const user = new User(req.body);
 
-    // mongoDB save
-    user.save((err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-            success: true,
-        });
+  // mongoDB save
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
     });
+  });
 });
 
 app.post("/api/users/login", (req, res) => {
-    // lookup DB to check if requested user email exists
+  // lookup DB to check if requested user email exists
 
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (!user) {
-            return res.json({
-                loginSuccess: false,
-                message: "user not found",
-            });
-        }
-        // check password
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch)
-                return res.json({ loginSuccess: false, message: "wrong password" });
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "user not found",
+      });
+    }
+    // check password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "wrong password" });
 
-            // generate token
-            user.generateToken((err, user) => {
-                if (err) return res.status(400).send(err);
-                // store token generated
-                res
-                    .cookie("x_auth", user.token)
-                    .status(200)
-                    .json({ loginSuccess: true, userId: user._id });
-            });
-        });
+      // generate token
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        // store token generated
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
+  });
 });
 
 app.get("/api/users/auth", auth, (req, res) => {
-    // been thru auth as true!
-    console.log("auth success");
-    res.status(200).json({
-        _id: req.user._id,
-        isAdmin: req.user.role === 0 ? false : true,
-        isAuth: true,
-        email: req.user.email,
-        firstName: req.user.firstName,
-        lastName: req.user.lastName,
-        role: req.user.role,
-        image: req.user.image,
-    });
+  // been thru auth as true!
+  console.log("auth success");
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    firstName: req.user.firstName,
+    lastName: req.user.lastName,
+    role: req.user.role,
+    image: req.user.image,
+  });
 });
 
 app.get("/api/users/logout", auth, (req, res) => {
-    User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).send({
-            success: true,
-        });
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
     });
+  });
 });
 
+const port = 5000;
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
